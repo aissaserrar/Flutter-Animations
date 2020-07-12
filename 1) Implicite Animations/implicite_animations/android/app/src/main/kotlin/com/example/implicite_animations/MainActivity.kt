ec0cb262f70e27a4cb347cc28e6dd224a841996f
Aissa <aissa.serrar95@gmail.com>
2020-07-12 10:42:15 +0100

    Implicite Animation Project initialization

---
+++ 1) Implicite Animations/implicite_animations/android/app/src/main/kotlin/com/example/implicite_animations/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.implicite_animations
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ 1) Implicite Animations/implicite_animations/lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Animation',
+      theme: ThemeData(
+        primarySwatch: Colors.blueGrey,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  HomePage({Key key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Implicit Animations',
+        ),
+      ),
+    );
+  }
+}
